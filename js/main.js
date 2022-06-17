c51d7063367d9564703eacebdc4b16e10666f307
Raul Vazquez <raul.vazquez.96@gmail.com>
2022-06-17 04:49:52 +0200

    v1 hackathon

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+//CAMBIAMOS EL TEXTO POR DEFECTO 
+$(document).ready(function(){
+    $('.dz-button').html('ARRASTRA TUS ARCHIVOS AQUI')
+    $('.dz-button').css({
+        "color": "white",
+        "font-family": 'Open Sans'
+        });
+  
+    
+})
+
+
