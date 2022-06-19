a1aac59bce7d674456088cf4d11931b558444c51
Raul Vazquez <raul.vazquez.96@gmail.com>
2022-06-19 21:58:48 +0200

    vistas responsive

--- js/main.js
+++ js/main.js
@@ -6,14 +6,14 @@ const button = document.querySelector('.btn-drop');
 const input = dropArea.querySelector('#input-file');
 
 let files;
-let archivo= false;
+let archivo = false;
 
 button.addEventListener('click', e => {
     console.log("click");
 });
 
 //Cada vez que cambia un archivo aplica la clase
-input.addEventListener("change", (e) => { 
+input.addEventListener("change", (e) => {
     files = this.files
     dropArea.classList.add("active");
     showFiles(files);
@@ -21,21 +21,21 @@ input.addEventListener("change", (e) => {
 })
 
 //Cuando arrastremos dentro de la zona
-dropArea.addEventListener("dragover", (e) => { 
+dropArea.addEventListener("dragover", (e) => {
     e.preventDefault();
     dropArea.classList.add("active")
     dragText.textContent = "Suelta tus archivos!"
 
 })
 //Cuando arrastremos fuera de la zona
-dropArea.addEventListener("dragleave", (e) => { 
+dropArea.addEventListener("dragleave", (e) => {
     e.preventDefault();
     dropArea.classList.remove("active");
     dragText.textContent = "Arrastra tus archivos aqui!"
 })
 
 //Cuando soltemos el archivo en la zona
-dropArea.addEventListener("drop", (e) => { 
+dropArea.addEventListener("drop", (e) => {
     e.preventDefault();
     files = e.dataTransfer.files;
     //console.log(files[0].name)
@@ -52,7 +52,7 @@ function showFiles(files) { //identificamos si hay mas de una imagen
     } else {
         for (const file of files) { //procesa el array         
             processFile(file);
-           // console.log(file.name)
+            // console.log(file.name)
         }
     }
 }
@@ -60,66 +60,68 @@ function showFiles(files) { //identificamos si hay mas de una imagen
 
 function processFile(file) {//validamos la imagen
     const docType = file.type;
-    const validExtensions = ['image/jpeg', 'image/jpg', 'image/png','image/gif'] ;
-    if(validExtensions.includes(docType)){
+    const validExtensions = ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'];
+    if (validExtensions.includes(docType)) {
         //archivo valido
         archivo = true;
         console.log(file.name)
-        listarArchivos(true,file.name);
-    }else{
+        listarArchivos(true, file.name);
+    } else {
         //mostrar error
         //alert('el archivo no es valido');
         error(true);
+        return
     }
 }
 
 //DIV LISTADO DE ARCHIVOS
-function listarArchivos(estado,nombre){
-    if(estado){
-    
-// Create an "p" node:
-const parrafo = document.createElement("p");
+function listarArchivos(estado, nombre) {
+    if (estado) {
 
-// Create a text node:
-const text = document.createTextNode(nombre);
+        // Create an "p" node:
+        const parrafo = document.createElement("p");
 
-// Append the text node to the "p" node:
-parrafo.appendChild(text);
+        // Create a text node:
+        const text = document.createTextNode(nombre);
 
-// Append the "p" node to the list:
-document.getElementById("listaArchivos").appendChild(parrafo);
+        // Append the text node to the "p" node:
+        parrafo.appendChild(text);
 
-dragText.style.display="none";
+        // Append the "p" node to the list:
+        document.getElementById("listaArchivos").appendChild(parrafo);
+
+        dragText.style.display = "none";
 
         document.getElementById("listaArchivos").style.color = "white";
-    }else{
+    } else {
         document.getElementById("listaArchivos").style.display = "none";
     }
 }
 
 
 //DIV DE ERROR
-function error(estado){ 
+function error(estado) {
 
     if (estado) {
-        dragText.style.display="none";
+        dragText.style.display = "none";
         document.getElementById("listaArchivos").style.display = "none";
-        document.getElementById("divError").style.display = "block"; 
-        
+        document.getElementById("divError").style.display = "block";
+
     } else {
         document.getElementById("divError").style.display = "none";
-        dragText.style.display="block";
+        dragText.style.display = "block";
         dragText.textContent = ""
     }
 }
 
 
-function subirArchivos(){
-    if(archivo){
+function subirArchivos() {
+    if (archivo) {
         document.getElementById("listaArchivos").style.display = "none";
-        dragText.style.display="none";
-        document.getElementById("success").style.display = "block"; 
-    }else{
-       error(true);
+        dragText.style.display = "none";
+        document.getElementById("success").style.display = "block";
+
+    } else {
+        error(true);
     }
 }
